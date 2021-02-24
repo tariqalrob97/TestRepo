9edc0bd01383154d3662e5eb185bb640e4c8eb62
zahera3 <47254909+zahera3@users.noreply.github.com>
2021-02-24 10:00:34 +0200

    Update LaunchApplication.java

--- TestNG/src/ChromeBrowser/LaunchApplication.java
+++ TestNG/src/ChromeBrowser/LaunchApplication.java
@@ -1,6 +1,7 @@
 package ChromeBrowser;
 import java.util.HashMap;
-
+import java.io.FileReader;
+import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -8,27 +9,39 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
 public class LaunchApplication {
+	
     
-
 	@Test
 	public void facebookTest()
 	{
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Zahera\\Downloads\\seleniumjar\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Zahera\\git\\TestRepo\\TestNG\\Driver\\chromedriver.exe");
 		final DesiredCapabilities dc = DesiredCapabilities.chrome();
+		Properties properties = new Properties();
+	       try(FileReader reader =  new FileReader("config")) {
+	           
+	           properties.load(reader);
+	          
+	          }catch (Exception e) {;
+	          e.printStackTrace();
+	          }
+	     
+		
 		dc.setCapability(ChromeOptions.CAPABILITY, new ChromeOptions() {
 		{
 		    setExperimentalOption("mobileEmulation", new HashMap<String, Object>() {
 		            {
-		                put("deviceName", "iPhone X");
+		                put("deviceName", properties.getProperty("deviceName"));
 		            }
 		        });
 		    }
 		});
 		
+		
 		WebDriver driver = new ChromeDriver(dc);
-		driver.get("https://www.ballarddesigns.com");
+		driver.get(properties.getProperty("siteURL"));
 	
 		System.out.println(driver.getTitle());
 	}
+	
 
 }
