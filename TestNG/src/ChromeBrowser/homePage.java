1ea23b299588891be1a6f4947186569fa30a01ac
zahera3 <47254909+zahera3@users.noreply.github.com>
2021-03-03 15:41:05 +0200

    Update homePage.java

--- TestNG/src/ChromeBrowser/homePage.java
+++ TestNG/src/ChromeBrowser/homePage.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Properties;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
@@ -16,6 +17,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.openqa.selenium.interactions.Actions;
 
 import Utils.LibraryUtils;
 
@@ -49,93 +51,96 @@ public class homePage {
 		
 		WebDriver driver = new ChromeDriver(dc);
 		driver.get(properties.getProperty("siteURL"));
-	
+		
+		try {
+			Thread.sleep(50000);
+		} 
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
 		System.out.println(driver.getTitle());
 		
+		
+		
 		try {//Test if the main elements are loaded correctly
+		
+		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#headerBox > div.t-header__bar > div > div.t-header-top.u-flexbox.u-width-full.u-padding-sm > div.u-flexbox.u-width-full.t-header-bar__title > div > a > div.pw-dangerous-html > div > svg")), 25);//logo
+		
+		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#headerBox > div.t-header__bar > div > div.t-header-top.u-flexbox.u-width-full.u-padding-sm > div.pw-header-bar__actions.t-header-bar__cart > button")), 25);//The order icon
 	
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#orderStatus > a > span")), 25);//The order icon
+		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#pwa-my-account-button > button")), 25);//account button
 		
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#wishlist > a")), 25);//wish list
+		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#headerBox > div.t-header__bar > div > div.t-header-bottom.u-flexbox.u-width-full > div:nth-child(2) > button")), 25);//search button
+	
+		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#pwa-shop-nav-button > button")), 25);//shop
 		
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#searchButton > img")), 25);//search button
+		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#main-link > img")), 25);// maim image
+
+		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#row-catgegoy-grid")), 25);// category grid
 		
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#searchBox")), 25);//search text box
+		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#module4 > div > div.row.edgey.flex-vertical-align > div.col-lg-9.col-sm-8.col-sm-7.col-xs-12")), 25);
+	
+    	LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#module4 > div > div.row.edgey.flex-vertical-align > div.col-lg-3.col-sm-4.col-xs-12.text-center")), 25);
 		
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#logo1 > a > img")), 25);//the logo
+		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#module4 > div > div.contentModule.template1.theme1 > div > div:nth-child(1)")), 25);
 		
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#bdheader > div > div.bottomline > div.left > div.DesignServices > a")), 25);//design services
-		
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#gwt-uid-72 > a > span.gwt-menuItem-link-categoryNameSplit2")), 25);// menu item1
+		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#field-0")), 25);//sign up by email textbox 
 
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#gwt-uid-135 > a")), 25);// menu item2
-		
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#gwt-uid-162 > a > span.gwt-menuItem-link-categoryNameSplit2")), 25);// menu item3
-		
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#gwt-uid-184 > a > span.gwt-menuItem-link-categoryNameSplit2")), 25);// menu item4
-		
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#gwt-uid-221 > a > span.gwt-menuItem-link-categoryNameSplit1")), 25);// menu item5
+		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#EmailSignUpForm > div > div.t-footer__newsletter-button-panel.u-margin-start-sm > button")), 25);//sign up by email button
 
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#gwt-uid-237 > a > span.gwt-menuItem-link-categoryNameSplit1")), 25);// menu item6
+		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#app-footer > footer > div.u-bg-color-neutral-00.u-margin-top-lg.u-margin-bottom-lg.u-padding-start-lg.u-padding-end-lg > div > button")), 25);// shipping location
 
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#gwt-uid-276 > a > span.gwt-menuItem-link-categoryNameSplit2")), 25);// menu item7
+    	LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#app-footer > footer > div.t-footer__phone.u-margin-bottom-lg.u-text-size-medium.u-flex-none > div.u-flexbox > div.t-footer__phone-number > a")), 25);//phone number
 
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#gwt-uid-285 > a > span.gwt-menuItem-link-categoryNameSplit2")), 25);// menu item8
+		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#app-footer > footer > div.t-footer__phone.u-margin-bottom-lg.u-text-size-medium.u-flex-none > div.c-egain__container > button > div > div > img")), 25);//chat
 
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#gwt-uid-294 > a > span.gwt-menuItem-link-categoryNameSplit2")), 25);// menu item9
+		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#app-footer > footer > div.t-footer__blogsection.u-flexbox > div > div > a:nth-child(1) > div")), 25);
 
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#gwt-uid-309 > a > span.gwt-menuItem-link-categoryNameSplit2")), 25);// menu item10
+		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#app-footer > footer > div.t-footer__blogsection.u-flexbox > div > div > a:nth-child(2) > div")), 25);
 
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#main-link > img")), 25);// main image
-
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#socialmedia")), 25);// social media area
-
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#customer-service-bar > div > div:nth-child(1) > p > a")), 25);// phone number
-
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#customer-service-bar > div > div:nth-child(2) > p > a")), 25);// chat
-
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#customer-service-bar > div > div:nth-child(3) > p > a")), 25);// email
-
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#customer-service-bar > div > div:nth-child(4) > p > a")), 25);// design services
-
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#gwt_country_changer > a > span")), 25);// shipping location
-
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#emailSignUp_label")), 25);// sign up by email label
-
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#emailSignUp")), 25);//  sign up by email text box
-
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#emailSignUpSubmitButton")), 25);//  sign up by email button
-		
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#footer")), 25);// the footer
-
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#row-catgegoy-grid")), 25);// grid
-
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#unit3 > div")), 25);
-		
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#footer > div.line3 > div.right > div.conditionsOfUse > a")), 25);//conditions Of Use
-
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#popularSearches > a")), 25);//popular searches 
-		
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#privacyAndSecurity > a")), 25);//privacy & security
-
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#siteMap > a")), 25);//site map
-
-		LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#sourceCode")), 25);//source code
-
-		
 		}
 		catch (NoSuchElementException | TimeoutException  ex) {
 	       // System.out.println("Some Elements do loaded");
 	        Assert .fail("Some Elements do loaded");
 
 	    }
+		
+		
 	
 		try {//Test the search process
 			
-			LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#searchBox")), 25).sendKeys(properties.getProperty("searchItem"));
-			LibraryUtils.waitForElementToBeClickable(driver, driver.findElement(By.cssSelector("#searchButton > img")), 25).click();
+		
+		    LibraryUtils.waitForElementToBeClickable(driver, driver.findElement(By.cssSelector("#headerBox > div.t-header__bar > div > div.t-header-bottom.u-flexbox.u-width-full > div:nth-child(2) > button")), 25).click();
 			
-			LibraryUtils.waitForElementToBeClickable(driver, driver.findElement(By.cssSelector("#unbxd-index-0 > div > div.gwt-product-info-panel-details-panel > div > div > h2 > a")), 25).click();//wait until the next page is loaded then click to an item
+			System.out.println(driver.getTitle());
+		
+			LibraryUtils.waitForElementToBeVisible(driver, driver.findElement(By.cssSelector("#search-0")), 25).sendKeys(properties.getProperty("searchItem"));
+			
+			try {
+				Thread.sleep(500);
+			} 
+			catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			LibraryUtils.waitForElementToBeClickable(driver, driver.findElement(By.cssSelector("#search-0")), 1).sendKeys(Keys.ENTER);
+			
+			try {
+				Thread.sleep(10000);
+			} 
+			catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			System.out.println(driver.getTitle());
+			
+			
+
+			//LibraryUtils.waitForElementToBeClickable(driver, driver.findElement(By.cssSelector("#app-main > div > div:nth-child(2) > div > div.pw-list.c-product-list-products.pw--borderless > article:nth-child(1) > div > div > div > div > div > article > a > div > div > img")), 25).click();
+			Actions act = new Actions(driver);
+			act.moveToElement(driver.findElement(By.xpath("//*[@id=\"app-main\"]/div/div[2]/div/div[2]/article[1]/div/div/div/div/div/article/a/div/div/img"))).click().build().perform();
+			
 			
 			
 		}
