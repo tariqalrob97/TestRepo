4ba24b40aae5c42f3ea000dc922fbae639d9bcd2
zahera3 <47254909+zahera3@users.noreply.github.com>
2021-03-04 11:16:02 +0200

    Add files via upload

---
+++ TestNG/src/Selectors/productSelectors.java
@@ -0,0 +1,6 @@
+package Selectors;
+
+public class productSelectors {
+	public static String addToCart="#aspnetForm > div.t-product-details.u-bg-color-neutral-10 > div:nth-child(3) > div > div > div.t-product-details-button > div.u-padding-top-lg > button > div";
+	public static String exitItemAdded="#sheet__header-2 > div > div.pw-header-bar__actions.c-header-bar__actions > button > div > div";
+}
---
+++ TestNG/src/Selectors/searchResultSelectors.java
@@ -0,0 +1,8 @@
+package Selectors;
+
+public class searchResultSelectors {
+	
+	public static String firstItem="#\\36 45396-1000 > div:nth-child(2) > a";
+
+
+}
